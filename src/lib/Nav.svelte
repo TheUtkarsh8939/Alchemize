900e44a12807d9ba7242351070178fa5a63cc975
Utkarsh Chandel <utkarshchandel2012@gmail.com>
2026-04-15 15:00:20 +0530

    Redesigned Landing Page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,9 +1,9 @@
-<div class="nav w-screen h-24 bg-grad absolute flex items-center pt-4 pl-10 justify-between pr-10">
+<div class="nav w-screen h-24 bg-grad absolute flex items-center pt-4 pl-10 justify-around pr-10">
         <img src="./Alchemist.webp" alt="" class="logo size-20">
         <div class="links text-gray-300">Links Coming Shortly...</div>
 </div>
 <style>
     .bg-grad{
-        background: linear-gradient(180deg, #00000000 20% , #000000 80%);
+        background: linear-gradient(180deg, #0000005e 20% , #000000 80%);
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,63 @@
 <script>
-    import Nav from '$lib/Nav.svelte';
+  import Nav from "$lib/Nav.svelte";
 </script>
+
 <Nav />
-<div class="absolute center-ovr w-screen h-screen flex items-center justify-center top-0">
-<div class="alchemy">
-    <h1 class=" text-red-500 text-5xl font-bold text-center mt-10 flex justify-center items-center">
-        <img src="./Alchemist.webp" alt="" class="logo size-20">
+<div
+  class="absolute center-ovr w-screen h-screen flex items-center justify-center top-0"
+>
+  <div class="alchemy flex items-center justify-center pb-10">
+    <div class="second-cont w-3xl">
+      <h1 class=" text-6xl flex items-center gap-10 font-bold">
+        <img src="./Alchemist.webp" alt="" class="logo size-16" />
         <span class="font alchemizefont">Alchemize</span>
-    </h1>
-    <div class="description">
-        <div class="flex items-center justify-center"><p class="text-gray-300 text-justify mt-10 text-lg w-4/5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique blanditiis velit natus, nisi accusamus quibusdam deleniti doloremque, possimus adipisci, eaque odio incidunt labore nemo at molestiae minus iusto ea inventore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt totam sapiente blanditiis molestiae eius. Ea laudantium reprehenderit ab expedita cum molestias doloremque? Sequi harum excepturi quae magnam vitae dolore minus.</p></div>
+      </h1>
+      <div class="description">
+        <div class="flex justify-center w-full flex-col">
+          <p
+            class="text-gray-300 text-justify mt-1 text-lg gap-2 flex flex-col"
+          >
+            <span class="text-3xl font-bold"
+              >3 Themes, 3 Currencies and a Dynamic Shop</span
+            >
+            <span class="text-xl">Under Draft | Hackclub Ages 13-18</span>
+          </p>
+          <div class="form flex">
+            <input
+              type="text"
+              placeholder="Enter your Email"
+              class="h-10 w-100 border border-red-500 mt-5 rounded-l-md text-gray-400 pl-2 outline-none"
+            />
+            <button class="h-10 w-28 bg-red-500 text-gray-200 font-bold border border-red-600 mt-5 rounded-r-md hover:bg-red-500 ml-1 text-md">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
-    
+<div class="absolute -z-10 w-screen h-screen">
+  <div class="img absolute -z-20 w-full h-full"></div>
+  <div class="ovr h-full w-screen -z-10"></div>
 </div>
-<div class="absolute -z-10 w-screen h-screen"><div class="img absolute -z-20 w-full h-full"></div><div class="ovr h-full w-screen -z-10"></div></div>
+
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap');
-    .img {
-        background-image: url('./bg.png');
-        background-size: cover;
-        background-position: center;
-    }
-    .ovr{
-        background: rgba(0, 0, 0, 0.541);
-    }
-    .alchemy{
-        background: radial-gradient(#00000028, #000000);
-        height: 500px;
-        width: 600px;
-        border: 1px solid #161616;
-        border-radius: 30px;
-    }
-    .alchemizefont{
-        font-family: 'Dancing Script', cursive;
-    }
+  @import url("https://fonts.googleapis.com/css2?family=Saira+Stencil:ital,wght@0,100..900;1,100..900&display=swap");
+  .img {
+    background-image: url("./bg2.png");
+    background-size: cover;
+    background-position: center;
+  }
+  .ovr {
+    background: rgba(0, 0, 0, 0.664);
+  }
+  .alchemy {
+    height: 100vh;
+    width: 50vw;
+  }
+  .alchemizefont {
+    font-family: "Saira Stencil", cursive;
+    color: hsl(0, 100%, 59%);
+  }
 </style>
